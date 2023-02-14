c8927cee8e3dc937c7d89adb6260376b7a003e2b
Shaharyar417 <shaharyar.bcss19@iba-suk.edu.pk>
2023-02-14 19:02:48 +0500

    header component added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './components/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-
+      <Header />
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
