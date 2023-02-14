c4cf522b5518e236424826fe8eb48feb5436d1d3
Shaharyar417 <shaharyar.bcss19@iba-suk.edu.pk>
2023-02-14 16:14:24 +0500

    updated menu-item with HOC withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1> HATS PAGE</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route path='/hats' element={<HatsPage />} />
+      </Routes>
+
     </div>
   );
 }
